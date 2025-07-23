168fa1af10c0784e49a279e2954304c4053d3e1e
Naimur00 <aminaim4u@gmail.com>
2025-07-23 16:44:46 +0600

    useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,22 @@ import Header from "./component/Header";
 import Main from "./component/Main";
 import ShoppingList from "./component/ShoppingList";
 import Counter from "./Counter";
+import CounterReducer from "./CounterReducer";
 import TodoList from "./TodoList";
 
 const App = () => {
   return (
-    <>
+    <main>
       <Header />
       {/* <Main /> */}
       {/* <Counter/> */}
       {/* <TodoList/> */}
       {/* <ShoppingList/> */}
       {/* <CopyInput/> */}
-      <Footer/>
-    </>
+      <CounterReducer/>
+      
+      {/* <Footer/> */}
+    </main>
   )
 };
 
--- src/CounterReducer.jsx
+++ src/CounterReducer.jsx
@@ -1,26 +1,46 @@
-import React from 'react';
-const initialState = {count: 0};
+import React, { useReducer, useState } from 'react';
+const initialState = { count: 0 };
 
 const reducer = (state, action) => {
   switch (action.type) {
     case 'increment':
-      return {count: state.count + 1}
+      return { count: state.count + 1 }
     case 'decrement':
-      return {count: state.count + 1}
+      return { count: state.count - 1 }
     case 'incrementByAmount':
-      return {count: state.count + 1}
+      return { count: state.count + action.payload }
     case 'decrementByAmount':
-      return {count: state.count + 1}
-  
-    default:
+      return { count: state.count - action.payload }
+    default: action.state;
       break;
   }
 }
 
 const CounterReducer = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [inputValue, setInputValue] = useState(0);
+
+  const handleIncrement = ()=> dispatch({type: 'increment'});
+  const handleDecrement = ()=> dispatch({type: 'decrement'});
+
+  const handleIncrementByAmount = ()=> {
+    dispatch({type: 'incrementByAmount', payload: +inputValue});
+    setInputValue(0);
+  }
+  const handleDecrementByAmount = ()=> {
+    dispatch({type: 'decrementByAmount', payload: +inputValue});
+    setInputValue(0);
+  }
+
   return (
     <div>
-      
+      <h2>Count: {state.count}</h2>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+      <br/>
+      <input type='number' value={inputValue} onChange={e => setInputValue(e.target.value)}/>
+      <button onClick={handleIncrementByAmount}>Increment by Amount</button>
+      <button onClick={handleDecrementByAmount}>Decrement by Amount</button>
     </div>
   );
 };
