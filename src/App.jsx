f7e6913c8ed1770e008fd0c72ad8d3439537f700
Naimur00 <aminaim4u@gmail.com>
2025-07-29 01:39:22 +0600

    all shoes data added

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import Form from "./component/Form";
 const App = () => {
   return (
     <main>
-      <Header />
+      {/* <Header /> */}
       {/* <Main /> */}
       {/* <Counter/> */}
       {/* <TodoList/> */}
