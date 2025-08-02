f5c1f524fbbbdea4b656e74825026bb703cacec4
Naimur00 <aminaim4u@gmail.com>
2025-08-03 03:07:44 +0600

    app and card

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,64 @@ import Nav from "./navigation/nav";
 import Products from "./products/Products";
 import Recommended from "./recommended/Recommended";
 import Sidebar from "./sidebar/Sidebar";
+import { useState } from "react";
+import data from "./db/data";
+import Card from "./component/Card";
 
 const App = () => {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  // INPUT FILTER
+  const [query, setQuery] = useState("");
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+  };
+  const filteredItems = data.filter((product) =>
+    product.title.toLocaleLowerCase().indexOf(query.toLocaleLowerCase() !== -1)
+  );
+
+  // RADIO FILTER
+  const handleChange = (e) => {
+    setSelectedCategory(e.target.value);
+  };
+
+  // BUTTONS FILTER
+  const handleClick = (e) => {
+    setSelectedCategory(e.target.value);
+  };
+
+  const filteredData = (products, selected, query) => {
+    let filteredProducts = products;
+
+    // FILTERING INPUT ITEMS
+    if (query) {
+      filteredProducts = filteredItems;
+    }
+
+    // SELECTED FILTERS
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+        ({ category, color, company, newPrice, title }) =>
+          category === selected ||
+          color === selected ||
+          company === selected ||
+          newPrice === selected ||
+          title === selected
+      );
+    }
+
+    return filteredData.map(({ img, title, star, reviews, prevPrice }) => (
+      <Card
+        key={Math.random()}
+        img={img}
+        title={title}
+        star={star}
+        reviews={reviews}
+        prevPrice={prevPrice}
+      />
+    ));
+  };
+
   return (
     <main className="max-w-5xl m-auto">
       <Sidebar />
